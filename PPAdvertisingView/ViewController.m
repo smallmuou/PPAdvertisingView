13a13dd62a19c99252d921a75f52a0aa8b67b5cb
许文发 <lvyexuwenfa100@126.com>
2015-07-08 14:21:39 +0800

    DEMO添加网络和本地图片

--- PPAdvertisingView/ViewController.m
+++ PPAdvertisingView/ViewController.m
@@ -21,39 +21,60 @@
     
     NSMutableArray* items = [NSMutableArray array];
     {
-        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"title 1" contentURL:@"http://www.baidu.com"];
+        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"本地图片1" contentURL:@"http://www.baidu.com"];
         item.image = [UIImage imageNamed:@"1.jpg"];
+        [items addObject:item];
+    }
+    
+    {
+        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"本地图片2" contentURL:@"http://www.baidu.com"];
+        item.image = [UIImage imageNamed:@"2.jpg"];
+        [items addObject:item];
+    }
+    
+    {
+        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"本地图片3" contentURL:@"http://www.baidu.com"];
+        item.image = [UIImage imageNamed:@"3.jpg"];
+        [items addObject:item];
+    }
+    
+    {
+        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"本地图片4" contentURL:@"http://www.baidu.com"];
+        item.image = [UIImage imageNamed:@"4.jpg"];
+        [items addObject:item];
+    }
+
+    {
+        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"网络图片1" contentURL:@"http://www.baidu.com"];
         item.imageURL = @"http://img02.tooopen.com/images/20141231/sy_78327074576.jpg";
         item.placeholderImage = [UIImage imageNamed:@"default.gif"];
         [items addObject:item];
     }
     
     {
-        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"title 2" contentURL:@"http://www.baidu.com"];
-        item.image = [UIImage imageNamed:@"2.jpg"];
+        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"网络图片2" contentURL:@"http://www.baidu.com"];
         item.placeholderImage = [UIImage imageNamed:@"default.gif"];
         item.imageURL = @"http://img02.tooopen.com/images/20150703/tooopen_sy_132841336778.jpg";
         [items addObject:item];
     }
     
     {
-        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"title 3" contentURL:@"http://www.baidu.com"];
-        item.image = [UIImage imageNamed:@"3.jpg"];
+        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"网络图片3" contentURL:@"http://www.baidu.com"];
         item.placeholderImage = [UIImage imageNamed:@"default.gif"];
         item.imageURL = @"http://img02.tooopen.com/images/20150703/tooopen_sy_132841942385.jpg";
         [items addObject:item];
     }
     
     {
-        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"title 4" contentURL:@"http://www.baidu.com"];
-        item.image = [UIImage imageNamed:@"4.jpg"];
+        PPAdvertisingItem* item = [PPAdvertisingItem itemWithTitle:@"网络图片4" contentURL:@"http://www.baidu.com"];
         item.placeholderImage = [UIImage imageNamed:@"default.gif"];
         item.imageURL = @"http://img02.tooopen.com/images/20150703/tooopen_sy_132839665724.jpg";
         [items addObject:item];
     }
+
     
     PPAdvertisingView* adView = [[PPAdvertisingView alloc] initWithFrame:CGRectMake(0, 50, self.view.bounds.size.width, 200) advertisingItems:items touchAction:^(PPAdvertisingItem *item) {
-        NSLog(@"Touch %@", item.title);
+        NSLog(@"点击 %@", item.title);
     }];
     
     [self.view addSubview:adView];
